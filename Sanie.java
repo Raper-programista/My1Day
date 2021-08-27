e999fc75b7813ccbcfa17429d1a63efbfc19013d
Raperprogramist <60778504+Raper-programista@users.noreply.github.com>
2021-08-27 12:42:56 +0000

    save adn check coordination

--- Sanie.java
+++ Sanie.java
@@ -1,7 +1,12 @@
+import static java.lang.Math.*;
+import java.util.*;
+
 public class Sanie{
 	private int x;
 	private int y;
 	private Degrees orientacja;
+	public Map<String, Integer>map = new HashMap<>();
+	public int count=0;
 
 	public Sanie(int x, int y, Degrees orientacja){
 		this.x = x;
@@ -55,10 +60,10 @@ public class Sanie{
 	}
 
 	private void goLeft(int steps){
-		if(orientacja.equals(Degrees.D0))			x -= steps;
-		if(orientacja.equals(Degrees.D90))		y += steps;
-		if(orientacja.equals(Degrees.D180))		x += steps;
-		if(orientacja.equals(Degrees.D270))		y -= steps;
+		if(orientacja.equals(Degrees.D0))			goStep(-steps, "x");
+		if(orientacja.equals(Degrees.D90))		goStep( steps, "y");
+		if(orientacja.equals(Degrees.D180))		goStep( steps, "x");
+		if(orientacja.equals(Degrees.D270))		goStep(-steps, "y");
 		
 		
 		orientacja = orientacja.previous();
@@ -66,11 +71,33 @@ public class Sanie{
 	}
 
 	private void goRight(int steps){
-		if(orientacja.equals(Degrees.D0))			x += steps;
-		if(orientacja.equals(Degrees.D90))		y -= steps;
-		if(orientacja.equals(Degrees.D180))		x -= steps;
-		if(orientacja.equals(Degrees.D270))		y += steps;
+		if(orientacja.equals(Degrees.D0))			goStep( steps, "x");
+		if(orientacja.equals(Degrees.D90))		goStep(-steps, "y");
+		if(orientacja.equals(Degrees.D180))		goStep(-steps, "x");
+		if(orientacja.equals(Degrees.D270))		goStep( steps, "y");
 
 		orientacja = orientacja.next();
+	}
+
+	public void goStep(int steps, String sign){
+
+		for (int i=0; i<abs(steps); i++){
+
+			if(steps>0 && sign.equals(x)) x++;
+			if(steps<0 && sign.equals(x)) x--;
+			if(steps>0 && sign.equals(y)) y++;
+			if(steps<0 && sign.equals(y)) y--;
+
+			save();
+		}
+
+	}
+	public void save(){
+
+			String XY = x+" "+y;
+
+			if(map.get(XY)==null)	map.put(XY, 1);
+			else	map.put(XY, map.get(XY)+1);
+			if(map.get(XY)==2) System.out.println(++count+". "+ XY+"<-");
 	}
 }
