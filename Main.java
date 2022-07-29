60872854927732610317175f5244f9980bcf8301
Raper-programista <60778504+Raper-programista@users.noreply.github.com>
2022-07-29 18:31:10 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,15 +4,16 @@ import static java.lang.Math.*;
 class Main {
   public static void main(String[] args) {
     Sanie s = new Sanie();
-		Sanie d = new Sanie();
-		String[] steps = Lista.getLista();
+    Sanie d = new Sanie();
+    String[] steps = Lista.getLista();
 
-	for(String step : steps){
-		Direction dir = Direction.parseDir(step.substring(0,1));
-		int numOfSteps = Integer.parseInt(step.substring(1));
+   for(String step : steps){
+	Direction dir = Direction.parseDir(step.substring(0,1));
+	int numOfSteps = Integer.parseInt(step.substring(1));
 
-		s.move(dir, numOfSteps);
-	}
+	s.move(dir, numOfSteps);
+   }
+	  
 	int x = abs(s.getX());
 	int y = abs(s.getY());
 	int result = x+y;	
